27cfd8d2cb12fec3c2a546084786a36b83f05577
Fleexie <jannickolsendk@hotmail.com>
2021-03-02 15:34:54 +0100

    Spaceship update, Small animations.

--- src/components/spaceship.js
+++ src/components/spaceship.js
@@ -2,60 +2,56 @@ import React from 'react';
 import {
     AppRegistry,
     StyleSheet,
-    Text,
     View,
-    Box,
     Cylinder,
-    Plane,
     Sphere,
-    asset
+
 } from 'react-360';
-import GeometryVR from "../../index";
-import Entity from 'Entity';
 
 export default class Spaceship extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        let movementUp = 2;
+        this.state = {
+            movementY: movementUp,
+            movementX: 1,
+            rotationY: -1,
+            movementZ: -.002,
+        }
+    }
     render() {
         return (
             <View>
-                {/*<View>*/}
-                {/*    <Cylinder radiusTop={0.8} radiusBottom={0.8} dimHeight={2} segments={12} style={{ color:'orange', transform: [{translate: [0, 0, 0]}]}}/>*/}
-                {/*    <Sphere radius={0.8} widthSegments={12} heightSegments={12}  style={{ color: 'orange', transform: [{ translate: [0,1,0] }] }}/>*/}
-                {/*</View>*/}
-                {/*<View>*/}
-                {/*    <Cylinder radiusTop={0.5} radiusBottom={0.5} dimHeight={2} segments={12} style={{ color:'white', transform: [{translate: [.7, -.3, 1]}]}}/>*/}
-                {/*    <Sphere radius={0.5} widthSegments={12} heightSegments={12}  style={{ color: 'white', transform: [{ translate: [.7,.7,1] }] }}/>*/}
-                {/*</View>*/}
-                {/*<View>*/}
-                {/*    <Cylinder radiusTop={0.2} radiusBottom={0.2} dimHeight={2} segments={6} style={{ color:'white', transform: [{translate: [-.3, -.3, .7]}]}}/>*/}
-                {/*    <Sphere radius={0.2} widthSegments={6} heightSegments={6}  style={{ color: 'white', transform: [{ translate: [-.3,.7,.7] }] }}/>*/}
-                {/*</View>*/}
+                <View style={{transform: [{translateY: this.state.movementY}, {translateX: this.state.movementX}, {translateZ: this.state.movementZ}, {rotateX: this.state.rotationY}, {rotateY: 90}]}}>
+                    <Cylinder radiusTop={.9} radiusBottom={1} dimHeight={5} segments={12} style={{ color:'lightgrey', transform: [{translate: [.7, -1.7, 1]}]}}/>
+                    <Cylinder radiusTop={.2} radiusBottom={.5} dimHeight={2} segments={12} style={{ color:'white', transform: [{translate: [.7, -4, 1]}]}}/>
+                    <Sphere radius={.9} widthSegments={12} heightSegments={12} style={{ color: 'white', transform: [{ translate: [.7,.7,1] }] }}/>
+                    <Cylinder radiusTop={1} radiusBottom={3} dimHeight={3} segments={2} style={{ color:'white', transform: [{translate: [.7, -2.5, 1]}, {rotateX: 0}, {rotateY: 0}, {rotateZ: 0}]}}/>
 
-                <View>
-                    <Cylinder radiusTop={2} radiusBottom={2} dimHeight={5} segments={12} style={{ color:'white', transform: [{translate: [.7, -.3, 1]}]}}/>
-                    <Sphere radius={2} widthSegments={12} heightSegments={12}  style={{ color: 'white', transform: [{ translate: [.7,.7,1] }] }}/>
+
+                    <View>
+                        <Cylinder radiusTop={.2} radiusBottom={.5} dimHeight={2} segments={9} style={{ color:'red',opacity:.3 , transform: [{translate: [.7, -5, 1]}]}}/>
+                        <Cylinder radiusTop={.2} radiusBottom={.7} dimHeight={2} segments={6} style={{ color:'yellow',opacity:.3 , transform: [{translate: [.7, -5.5, 1]}]}}/>
+                    </View>
                 </View>
             </View>
         );
     }
+
+    componentDidMount() {
+        this.movementY = setInterval(() => {
+            this.setState({movementY: this.state.movementY + .005});
+            this.setState({movementX: this.state.movementX + .004});
+            this.setState({movementZ: this.state.movementZ - .008});
+
+            if(this.state.rotationY >= -61){
+                this.setState({rotationY: this.state.rotationY - .09});
+            }
+
+        }, 10);
+    }
 };
 
-const styles = StyleSheet.create({
-    panel: {
-        // Fill the entire surface
-        width: 1000,
-        height: 600,
-        backgroundColor: 'rgba(255, 255, 255, 0.4)',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    greetingBox: {
-        padding: 20,
-        backgroundColor: '#000000',
-        borderColor: '#639dda',
-        borderWidth: 2,
-    },
-    greeting: {
-        fontSize: 30,
-    },
-});
 AppRegistry.registerComponent('Spaceship', () => Spaceship);
