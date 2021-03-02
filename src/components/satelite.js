91f1f0649acf4371702dae8d2ed0e2e370f700c3
Hazeha <krantja@gmail.com>
2021-03-02 10:40:23 +0100

    Cleanup

--- src/components/satelite.js
+++ src/components/satelite.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import {
     AppRegistry,
-    StyleSheet,
-    Text,
     View,
-    Box,
     Cylinder,
-    Plane,
-    Sphere,
-    asset
 } from 'react-360';
-import {rotateByQuaternion} from "react-360-web/js/Utils/Math";
 
 export default class Satelite extends React.Component {
     render() {
@@ -28,24 +21,4 @@ export default class Satelite extends React.Component {
         );
     }
 };
-
-const styles = StyleSheet.create({
-    panel: {
-        // Fill the entire surface
-        width: 1000,
-        height: 600,
-        backgroundColor: 'rgba(255, 255, 255, 0.4)',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    greetingBox: {
-        padding: 20,
-        backgroundColor: '#000000',
-        borderColor: '#639dda',
-        borderWidth: 2,
-    },
-    greeting: {
-        fontSize: 30,
-    },
-});
 AppRegistry.registerComponent('Satelite', () => Satelite);
--- src/components/ufo.js
+++ src/components/ufo.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import {
     AppRegistry,
-    StyleSheet,
-    Text,
     View,
-    Box,
     Cylinder,
-    Plane,
-    Sphere,
-    asset
+    Sphere
 } from 'react-360';
-import GeometryVR from "../../index";
-import Entity from 'Entity';
+
 
 
 export default class UFO extends React.Component {
@@ -24,24 +18,4 @@ export default class UFO extends React.Component {
         );
     }
 };
-
-const styles = StyleSheet.create({
-    panel: {
-        // Fill the entire surface
-        width: 1000,
-        height: 600,
-        backgroundColor: 'rgba(255, 255, 255, 0.4)',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    greetingBox: {
-        padding: 20,
-        backgroundColor: '#000000',
-        borderColor: '#639dda',
-        borderWidth: 2,
-    },
-    greeting: {
-        fontSize: 30,
-    },
-});
 AppRegistry.registerComponent('UFO', () => UFO);
