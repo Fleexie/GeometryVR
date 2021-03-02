9600fc6f7a2bf442e85a4f187a42ef489b740a2d
Hazeha <krantja@gmail.com>
2021-03-02 10:28:02 +0100

    Sat and ufo setup

--- src/components/satelite.js
+++ src/components/satelite.js
@@ -10,12 +10,20 @@ import {
     Sphere,
     asset
 } from 'react-360';
+import {rotateByQuaternion} from "react-360-web/js/Utils/Math";
 
 export default class Satelite extends React.Component {
     render() {
         return (
             <View>
-                <Sphere radius={2} widthSegments={12} heightSegments={12}  style={{ color: 'green', transform: [{ translate: [.7,.7,1] }] }}/>
+                {/* Main Parts */}
+                <Cylinder radiusTop={0.9} radiusBottom={0.9} dimHeight={5} segments={24} style={{ color:'black', transform: [{translate: [.7,.7,1]}]}}/>
+                <Cylinder radiusTop={0.4} radiusBottom={0.4} dimHeight={3} segments={24} style={{ color:'darkblue', transform: [{translate: [-2,.7,1]}]}}/>
+                <Cylinder radiusTop={0.4} radiusBottom={0.4} dimHeight={3} segments={24} style={{ color:'darkblue', transform: [{translate: [3,.7,1]}]}}/>
+
+                {/* Small Tubes */}
+                <Cylinder radiusTop={0.1} radiusBottom={0.1} dimHeight={2} segments={24} style={{ color:'grey', transform: [{translate: [2,1,1]}, {rotateZ: 90}]}}/>
+                <Cylinder radiusTop={0.1} radiusBottom={0.1} dimHeight={2} segments={24} style={{ color:'grey', transform: [{translate: [-1,1,1]}, {rotateZ: 90}]}}/>
             </View>
         );
     }
--- src/components/ufo.js
+++ src/components/ufo.js
@@ -18,7 +18,8 @@ export default class UFO extends React.Component {
     render() {
         return (
             <View>
-                <Sphere radius={2} widthSegments={12} heightSegments={12}  style={{ color: 'yellow', transform: [{ translate: [.7,.7,1] }] }}/>
+                <Sphere radius={1} widthSegments={12} heightSegments={12}  style={{ color: 'black', transform: [{ translate: [.7,.7,1] }] }}/>
+                <Cylinder radiusTop={2} radiusBottom={2} dimHeight={0.3} segments={24} style={{ color:'darkgrey', transform: [{translate: [.7,.7,1]}]}}/>
             </View>
         );
     }
